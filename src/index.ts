38faf8a82daadd548b6412176868f3064a18250f
PADO <62028267+47PADO47@users.noreply.github.com>
2023-01-21 20:45:02 +0100

    ✨ Add StorageEnv export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import R2Manager from "./core/R2Manager";
 import CacheManager from "./core/CacheManager";
 import KVManager from "./core/KVManager";
+import Env from "./types/Env";
+
+export type { 
+    Env as StorageEnv
+};
 
 export {
     R2Manager,
     CacheManager,
-    KVManager,
-}
+    KVManager
+};
